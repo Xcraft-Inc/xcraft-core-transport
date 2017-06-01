e7c96b9f074773aa1fc2dfffbaf9eaae2adda597
Mathieu Schroeter <mathieu@schroetersa.ch>
2017-06-01 16:15:54 +0200

    Add a test for pubsub

--- test/router.spec.js
+++ test/router.spec.js
@@ -36,3 +36,44 @@ describe ('pushpull', function () {
     });
   });
 });
+
+describe ('pubsub', function () {
+  const server = new Router ('pub', xLog);
+  const clientEe = new Router ('sub', xLog);
+  const clientAxon = new Router ('sub', xLog);
+
+  it ('#subscribe and publish', function (done) {
+    let cnt = 0;
+    const _done = () => {
+      ++cnt;
+      if (cnt === 2) {
+        clientEe.stop ();
+        clientAxon.stop ();
+        server.stop ();
+        done ();
+      }
+    };
+
+    server.start ({host: '127.0.0.1', port: 3335}, () => {
+      clientEe.subscribe ('foobar');
+      clientEe.on ('message', (...args) => {
+        expect (args.length).to.be.eql (2);
+        expect (args[0]).to.be.eql ('foobar');
+        expect (args[1]).to.be.eql ('the message');
+        _done ();
+      });
+      clientEe.connect ('ee', {});
+
+      clientAxon.subscribe ('foobar');
+      clientAxon.on ('message', (...args) => {
+        expect (args.length).to.be.eql (2);
+        expect (args[0]).to.be.eql ('foobar');
+        expect (args[1]).to.be.eql ('the message');
+        _done ();
+      });
+      clientAxon.connect ('axon', {port: 3335, host: '127.0.0.1'});
+
+      server.send ('foobar', 'the message');
+    });
+  });
+});
