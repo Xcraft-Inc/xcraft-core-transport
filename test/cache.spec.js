147120ed0981fe0647b13436ed6f994e75230850
Mathieu Schroeter <mathieu@schroetersa.ch>
2021-08-10 09:19:05 +0200

    Adapt tests according to the new ids extractor

--- test/cache.spec.js
+++ test/cache.spec.js
@@ -5,6 +5,34 @@ const {extractIds} = require('../lib/helpers.js');
 const Cache = require('../lib/cache.js');
 
 describe('cache', function () {
+  it('#extractIds for a simple line', function () {
+    const ids = extractIds('albert@levert::my@entity.<with-a-line>');
+
+    expect(ids[0]).to.be.equal('_');
+    expect(ids[1]).to.be.equal('<with-a-line>');
+    expect(ids[2]).to.be.equal('albert@levert');
+    expect(ids[3]).to.be.equal('my@entity');
+  });
+
+  it('#extractIds for line with id', function () {
+    const ids = extractIds('albert@levert::my@entity.<its@an@id>');
+
+    expect(ids[0]).to.be.equal('_');
+    expect(ids[1]).to.be.equal('albert@levert');
+    expect(ids[2]).to.be.equal('my@entity');
+    expect(ids[3]).to.be.equal('its@an@id');
+  });
+
+  it('#extractIds for command', function () {
+    const ids = extractIds(
+      'albert@levert::action.abc-abc-abc-abc-abc.finished'
+    );
+
+    expect(ids[0]).to.be.equal('_');
+    expect(ids[1]).to.be.equal('albert@levert');
+    expect(ids[2]).to.be.equal('.abc-abc-abc-abc-abc.');
+  });
+
   it('#matches global', function () {
     const cache = new Cache();
     let r;
@@ -30,15 +58,15 @@ describe('cache', function () {
 
     r = /.*::a@a/;
     ids = extractIds('z@z::a@a');
-    id = ids.length > 1 ? ids[1] : ids[0];
+    id = ids[ids.length - 1];
     cache.set(id, r.toString(), r);
     r = /.*::b.*/;
     ids = extractIds('z@z::b');
-    id = ids.length > 1 ? ids[1] : ids[0];
+    id = ids[ids.length - 1];
     cache.set(id, r.toString(), r);
     r = /.*::.*/;
     ids = extractIds('a::a');
-    id = ids.length > 1 ? ids[1] : ids[0];
+    id = ids[ids.length - 1];
     cache.set(id, r.toString(), r);
 
     expect(cache.matches('test::a@a')).to.be.true;
